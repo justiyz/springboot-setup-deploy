fe9efafda534597d96704f1539c895db1e2a89bf
justiyz <inyange@yahoo.com>
2021-02-04 01:14:27 +0100

    adding modules

---
+++ web/src/main/java/com/springbootsetup/SpringbootsetupApplication.java
@@ -0,0 +1,13 @@
+package com.springbootsetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootsetupApplication.class, args);
+	}
+
+}
---
+++ web/src/test/java/com/springbootsetup/SpringbootsetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootsetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootsetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
